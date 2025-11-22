c4fbca66b0c469ca0446efe8ce10ff21974dcd5b
Gerson Jean Pierre <jalukone@outlook.com>
2025-11-22 18:40:53 -0500

    implement guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
+import { authGuard, authRedirectGuard } from '@core/auth/guards/auth-guard';
 export const routes: Routes = [
   {
+    path: 'auth',
+    canActivate: [authRedirectGuard],
+    loadChildren: () => import('./features/auth/auth.routes'),
+  },
+  {
     path: '',
+    canActivate: [authGuard],
     loadComponent: () => import('./layout/main-layout/main-layout'),
-    children: [],
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
   },
 ];
