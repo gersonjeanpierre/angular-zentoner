b42016efc7c0c6dbd267aa726bc0cc19c888fcbe
Gerson Jean Pierre <jalukone@outlook.com>
2025-11-26 14:44:51 -0500

    chore: untrack environment files

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  development: true,
-  SUPABASE_URL: 'http://localhost:3000',
-  SUPABASE_ANON_KEY: 'anon-key',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  SUPABASE_URL: 'http://localhost:3000',
-  SUPABASE_ANON_KEY: 'anon-key',
-};
